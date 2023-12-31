23e267cbadaf35174f708128047265cff7084f56
Md. Naim Chowdhury <naimmcduck@gmail.com>
2023-12-31 10:32:14 +0100

    remove the unused session from order template

--- resources/views/Pizzaplaza/order.blade.php
+++ resources/views/Pizzaplaza/order.blade.php
@@ -4,12 +4,6 @@
     <x-article />
 
     <div class="container my-5">
-        @if(session('cart'))
-            <div class="alert alert-success text-center">
-                Pizza has been Added!
-            </div>
-        @endif
-
         <table class="table table-hover mt-3 text-center" id="myTable">
         </table>
     </div>
